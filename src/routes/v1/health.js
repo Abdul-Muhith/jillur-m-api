f074ce307a09ed5af943cc159a3b96286b8d8647
Abdul Muhith <amuhith.mern@outlook.com>
2024-10-18 20:55:40 +0600

    Added local-health endpoint successfully

--- src/routes/v1/health.js
+++ src/routes/v1/health.js
@@ -3,9 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (_req, res) => {
-  console.log("GET /api/v1/local/health");
-
-  res.status(200).json({ health: "OK" });
+  res.status(200).json({
+    code: 200,
+    message: "The service is operational and functioning properly.",
+    data: {
+      status: true,
+      uptime: process.uptime(), // Uptime in seconds
+      timestamp: new Date().toISOString(),
+    },
+  });
 });
 
 export default router;
